0164df5ba817a3c77061ed7253a05f9b96a8d17a
Daniel Aditya Istyana <adityadaniel@daniels-macbook-pro.local>
2018-07-25 20:43:02 +0700

    Initial Commit

---
+++ TxtCases/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TxtCases
+//
+//  Created by Daniel Aditya Istyana on 25/07/18.
+//  Copyright © 2018 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
